ccf4e603dba283778c0a06713ef061e0a6a21aa3
Mckenzie Nicol <mnicol11@my.bcit.ca>
2024-03-08 12:33:05 -0800

    Update index layout... again Add logo image Add custom colours to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,14 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pokemon: ['Pokemon Solid', 'sans-serif'],
+        pokemon: ["Pokemon Solid", "sans-serif"],
+      },
+      colors: {
+        onyx: "#3A4041ff",
+        charcoal: "#3B4F67ff",
+        biceblue: "#316E95ff",
+        honeydew: "#E3EBDBff",
+        sage: "#AFAE60ff",
       },
     },
   },
