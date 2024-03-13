57f4a8d0685514fd7074b80426c8b64f7e950160
jake currie <jcurrie42@my.bcit.ca>
2024-03-13 14:44:20 -0700

    add trainer interface and get functions for fetching trainers

---
+++ app/models/trainers.server.ts
@@ -0,0 +1,26 @@
+import arc from "@architect/functions";
+
+export interface Trainer {
+  id: string;
+  name: string;
+  collectionId: string;
+  imageURL: string;
+}
+
+export async function getAllTrainers(): Promise<Trainer[]> {
+  const db = await arc.tables();
+  const result = await db.trainers.scan({});
+  if (!result.Items) {
+    return [];
+  }
+  return result.Items.map(item => item as Trainer);
+}
+
+export async function getTrainerById(id: string): Promise<Trainer | null> {
+  const db = await arc.tables();
+  const result = await db.trainers.get({ id });
+  if (!result) {
+    return null;
+  }
+  return result as Trainer;
+}
