7f9f63c1b8494c24931575770012ff577b1a764f
jake currie <jcurrie42@my.bcit.ca>
2024-03-20 11:11:46 -0700

    fix redirect route

--- app/routes/battle.$trainerId.tsx
+++ app/routes/battle.$trainerId.tsx
@@ -62,7 +62,7 @@ export default function BattleTrainerId() {
 
   useEffect(() => {
     if (!selectedTrainer) {
-      throw redirect("/battle-setup");
+      throw redirect("/battle");
     }
   }, [selectedTrainer]);
 
