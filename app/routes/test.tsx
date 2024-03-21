12b60556ab3bae22c8f4a79a063e8d07430a8f45
jake currie <jcurrie42@my.bcit.ca>
2024-03-21 14:16:22 -0700

    testing out battle component

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,9 +1,21 @@
-import { TimingSlider } from "~/components/timingSlider";
+import { Outlet } from "@remix-run/react";
+import BattleComponent from "../components/battleDisplay";
 
-export default function Test() {
+const playerTeam = [
+  { name: "Pikachu", hp: 150, attack: 80 },
+  { name: "Charmander", hp: 140, attack: 60 },
+];
+
+const enemyTeam = [
+  { name: "Bulbasaur", hp: 110, attack: 55 },
+  { name: "Squirtle", hp: 130, attack: 65 },
+];
+
+export default function BattleTestRoute() {
   return (
     <div>
-      <TimingSlider />
+      <BattleComponent playerTeam={playerTeam} enemyTeam={enemyTeam} />
+      <Outlet />
     </div>
   );
 }
