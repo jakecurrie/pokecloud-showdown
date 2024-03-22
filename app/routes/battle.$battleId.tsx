9fae1527a1144d444851c8ad7f025812defe48b5
Mckenzie Nicol <mnicol11@my.bcit.ca>
2024-03-21 17:09:10 -0700

    Implement battle with chosen pokemon

--- app/routes/battle.$battleId.tsx
+++ app/routes/battle.$battleId.tsx
@@ -1,11 +1,18 @@
 import { ActionFunction, json } from "@remix-run/node";
-import { redirect, useActionData } from "@remix-run/react";
+import { Outlet, redirect, useActionData } from "@remix-run/react";
 
 import { getTrainerCollectionWithPokemonDetails } from "~/models/trainers.server";
 import { requireUserId } from "~/session.server";
 import { getPokemonById, Pokemon } from "~/models/pokemon.server";
 import { createBattle } from "~/models/battle.server";
 import PokemonCard from "~/components/pokecard";
+import BattleComponent from "../components/battleDisplay";
+
+interface BattleDetails {
+  name: string;
+  hp: number;
+  attack: number;
+}
 
 export const action: ActionFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -25,30 +32,98 @@ export const action: ActionFunction = async ({ request }) => {
   const poke3 = await getPokemonById(battleData.pokemonId3.toString());
   const poke4 = await getPokemonById(battleData.pokemonId4.toString());
   const poke5 = await getPokemonById(battleData.pokemonId5.toString());
-  const trainerCollection =
+  const trainerPokemon =
     await getTrainerCollectionWithPokemonDetails(trainerId);
+
+  let trainerCollection: Pokemon[] = [];
+  if (
+    trainerPokemon[0].pokemon &&
+    trainerPokemon[1].pokemon &&
+    trainerPokemon[2].pokemon &&
+    trainerPokemon[3].pokemon &&
+    trainerPokemon[4].pokemon
+  ) {
+    trainerCollection = [
+      trainerPokemon[0].pokemon,
+      trainerPokemon[1].pokemon,
+      trainerPokemon[2].pokemon,
+      trainerPokemon[3].pokemon,
+      trainerPokemon[4].pokemon,
+    ];
+  }
 
   let userCollection: Pokemon[] = [];
 
   if (poke1 && poke2 && poke3 && poke4 && poke5 && trainerCollection) {
     userCollection = [poke1, poke2, poke3, poke4, poke5];
 
-    console.log("Creating battle");
+    let trainerBattleCollection: BattleDetails[] = [];
+    if (
+      trainerPokemon[0].pokemon &&
+      trainerPokemon[1].pokemon &&
+      trainerPokemon[2].pokemon &&
+      trainerPokemon[3].pokemon &&
+      trainerPokemon[4].pokemon
+    ) {
+      trainerBattleCollection = [
+        {
+          name: trainerPokemon[0].pokemon.name,
+          hp: trainerPokemon[0].pokemon.hp,
+          attack: trainerPokemon[0].pokemon.hp / 2,
+        },
+        {
+          name: trainerPokemon[1].pokemon.name,
+          hp: trainerPokemon[1].pokemon.hp,
+          attack: trainerPokemon[1].pokemon.hp / 2,
+        },
+        {
+          name: trainerPokemon[2].pokemon.name,
+          hp: trainerPokemon[2].pokemon.hp,
+          attack: trainerPokemon[2].pokemon.hp / 2,
+        },
+        {
+          name: trainerPokemon[3].pokemon.name,
+          hp: trainerPokemon[3].pokemon.hp,
+          attack: trainerPokemon[3].pokemon.hp / 2,
+        },
+        {
+          name: trainerPokemon[4].pokemon.name,
+          hp: trainerPokemon[4].pokemon.hp,
+          attack: trainerPokemon[4].pokemon.hp / 2,
+        },
+      ];
+    }
 
-    await createBattle({
-      userId: userId as string,
-      battleId: battleId as string,
-      trainerId: trainerId as string,
-      won: 0,
+    let userBattleCollection: BattleDetails[] = [];
+
+    if (poke1 && poke2 && poke3 && poke4 && poke5 && trainerBattleCollection) {
+      userBattleCollection = [
+        { name: poke1.name, hp: poke1.hp, attack: poke1.hp / 2 },
+        { name: poke2.name, hp: poke2.hp, attack: poke2.hp / 2 },
+        { name: poke3.name, hp: poke3.hp, attack: poke3.hp / 2 },
+        { name: poke4.name, hp: poke4.hp, attack: poke4.hp / 2 },
+        { name: poke5.name, hp: poke5.hp, attack: poke5.hp / 2 },
+      ];
+
+      console.log("Creating battle");
+
+      await createBattle({
+        userId: userId as string,
+        battleId: battleId as string,
+        trainerId: trainerId as string,
+        won: 0,
+      });
+
+      console.log("Created battle");
+    }
+    redirect(`/battle/${battleId}`);
+    return json({
+      userCollection: userCollection,
+      trainerCollection: trainerCollection,
+      userBattleCollection: userBattleCollection,
+      trainerBattleCollection: trainerBattleCollection,
     });
-
-    console.log("Created battle");
   }
-  redirect(`/battle/${battleId}`);
-  return json({
-    userCollection: userCollection,
-    trainerCollection: trainerCollection,
-  });
 };
 
 export default function BattleTrainerId() {
@@ -57,7 +132,7 @@ export default function BattleTrainerId() {
   return (
     <div>
       <h4>YOUR POKEMON:</h4>
-      <div className={"grid grid-cols-5"}>
+      <div className={"grid grid-cols-5 w-1/2"}>
         <PokemonCard
           name={actionData.userCollection[0].name}
           imageUrl={actionData.userCollection[0].image_url}
@@ -96,43 +171,48 @@ export default function BattleTrainerId() {
       </div>
 
       <h4>TRAINER POKEMON:</h4>
-      <div className={"grid grid-cols-5"}>
+      <div className={"grid grid-cols-5 w-1/2"}>
         <PokemonCard
-          name={actionData.trainerCollection[0].pokemon.name}
-          imageUrl={actionData.trainerCollection[0].pokemon.image_url}
-          type={actionData.trainerCollection[0].pokemon.type}
-          hp={actionData.trainerCollection[0].pokemon.hp}
-          attack={actionData.trainerCollection[0].pokemon.attack}
+          name={actionData.trainerCollection[0].name}
+          imageUrl={actionData.trainerCollection[0].image_url}
+          type={actionData.trainerCollection[0].type}
+          hp={actionData.trainerCollection[0].hp}
+          attack={actionData.trainerCollection[0].attack}
         />
         <PokemonCard
-          name={actionData.trainerCollection[1].pokemon.name}
-          imageUrl={actionData.trainerCollection[1].pokemon.image_url}
-          type={actionData.trainerCollection[1].pokemon.type}
-          hp={actionData.trainerCollection[1].pokemon.hp}
-          attack={actionData.trainerCollection[1].pokemon.attack}
+          name={actionData.trainerCollection[1].name}
+          imageUrl={actionData.trainerCollection[1].image_url}
+          type={actionData.trainerCollection[1].type}
+          hp={actionData.trainerCollection[1].hp}
+          attack={actionData.trainerCollection[1].attack}
         />
         <PokemonCard
-          name={actionData.trainerCollection[2].pokemon.name}
-          imageUrl={actionData.trainerCollection[2].pokemon.image_url}
-          type={actionData.trainerCollection[2].pokemon.type}
-          hp={actionData.trainerCollection[2].pokemon.hp}
-          attack={actionData.trainerCollection[2].pokemon.attack}
+          name={actionData.trainerCollection[2].name}
+          imageUrl={actionData.trainerCollection[2].image_url}
+          type={actionData.trainerCollection[2].type}
+          hp={actionData.trainerCollection[2].hp}
+          attack={actionData.trainerCollection[2].attack}
         />{" "}
         <PokemonCard
-          name={actionData.trainerCollection[3].pokemon.name}
-          imageUrl={actionData.trainerCollection[3].pokemon.image_url}
-          type={actionData.trainerCollection[3].pokemon.type}
-          hp={actionData.trainerCollection[3].pokemon.hp}
-          attack={actionData.trainerCollection[3].pokemon.attack}
+          name={actionData.trainerCollection[3].name}
+          imageUrl={actionData.trainerCollection[3].image_url}
+          type={actionData.trainerCollection[3].type}
+          hp={actionData.trainerCollection[3].hp}
+          attack={actionData.trainerCollection[3].attack}
         />{" "}
         <PokemonCard
-          name={actionData.trainerCollection[4].pokemon.name}
-          imageUrl={actionData.trainerCollection[4].pokemon.image_url}
-          type={actionData.trainerCollection[4].pokemon.type}
-          hp={actionData.trainerCollection[4].pokemon.hp}
-          attack={actionData.trainerCollection[4].pokemon.attack}
+          name={actionData.trainerCollection[4].name}
+          imageUrl={actionData.trainerCollection[4].image_url}
+          type={actionData.trainerCollection[4].type}
+          hp={actionData.trainerCollection[4].hp}
+          attack={actionData.trainerCollection[4].attack}
         />
       </div>
+      <BattleComponent
+        enemyTeam={actionData.trainerBattleCollection}
+        playerTeam={actionData.userBattleCollection}
+      />
+      <Outlet />
     </div>
   );
 }
