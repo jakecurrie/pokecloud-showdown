7ea9d0fc4e6625ad5b50a68d59a01a238bd277aa
jake currie <jcurrie42@my.bcit.ca>
2024-04-01 20:17:26 -0700

    use link instead of a tag for navbar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,53 +1,55 @@
-import logoPic from "../../public/images/logo.png";
+import { Link } from "@remix-run/react";
+
+import logo from "../../public/images/logo.png"
 
 const NavBar = () => {
   return (
     <nav className="absolute inset-0 bg-biceblue border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="/home"
+        <Link
+          to="/home"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img src={logoPic} className="h-8" alt="Pokecloud logo" />
+          <img src={logo} className="h-8" alt="Pokecloud logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             PokeCloud
           </span>
-        </a>
+        </Link>
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li className={"px-4"}>
-              <a
-                href="/home"
+            <li className="px-4">
+              <Link
+                to="/home"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
-            <li className={"px-4"}>
-              <a
-                href="/collections"
+            <li className="px-4">
+              <Link
+                to="/collections"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 MyCards
-              </a>
+              </Link>
             </li>
-            <li className={"px-4"}>
-              <a
-                href="/battle"
+            <li className="px-4">
+              <Link
+                to="/battle"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Battle
-              </a>
+              </Link>
             </li>
-            <li className={"px-4"}>
-              <a
-                href="/logout"
+            <li className="px-4">
+              <Link
+                to="/logout"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Logout
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
